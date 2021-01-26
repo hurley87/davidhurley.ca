e8e4dff7c6a78f4be82058b2f0ca11d53bb71d37
david hurley <dhurls99@gmail.com>
2021-01-26 14:46:53 -0500

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>David Hurley</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>David Hurley</h1>
+      <main>
+        <h1>David Hurley</h1>
       </main>
     </div>
   );
