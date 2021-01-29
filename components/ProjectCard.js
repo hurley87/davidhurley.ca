302403939f15e9a4a86f52cfd7ac6f5db154b0f7
david hurley <dhurls99@gmail.com>
2021-01-29 17:01:13 -0500

    update fathom goals

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -2,8 +2,8 @@ import * as Fathom from 'fathom-client';
 
 const trackGoal = (title) => {
   const goalCodes = {
-    'React 2025': '5WGDOKV0',
-    'Mastering Next.js': 'HV9HDL0O'
+    'Sales Captain': '5WGDOKV0',
+    Conversify: 'MQOBUTOP'
   };
 
   Fathom.trackGoal(goalCodes[title], 0);
